a7ee11ef6a38a1409854d266debb3c842f7c4203
Diego Melo <di.sinfo35@gmail.com>
2025-06-20 01:08:47 -0300

    Feat: add arquivos para conexão e imagens tela login + pasta painel

---
+++ conexao.php
@@ -0,0 +1,55 @@
+<?php 
+
+//dados conexão bd local
+$servidor = 'localhost';
+$banco = 'projetoLogin';
+$usuario = 'root';
+$senha = '';
+
+try {
+	$pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", "$usuario", "$senha");
+} catch (Exception $e) {
+	echo 'Erro ao conectar ao banco de dados!<br>';
+	echo $e;
+}
+
+
+//variaveis globais
+$nome_sistema = 'Nome Sistema';
+$email_sistema = 'di.sinfo35@gmail.com';
+$telefone_sistema = '(11)98489-6994';
+
+$query = $pdo->query("SELECT * from config");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$linhas = @count($res);
+if($linhas == 0){
+	$pdo->query("INSERT INTO config SET nome = '$nome_sistema', email = '$email_sistema', telefone = '$telefone_sistema', logo = 'logo.png', logo_rel = 'logo.jpg', icone = 'icone.png', ativo = 'Sim'");
+}else{
+$nome_sistema = $res[0]['nome'];
+$email_sistema = $res[0]['email'];
+$telefone_sistema = $res[0]['telefone'];
+$endereco_sistema = $res[0]['endereco'];
+$instagram_sistema = $res[0]['instagram'];
+$logo_sistema = $res[0]['logo'];
+$logo_rel = $res[0]['logo_rel'];
+$icone_sistema = $res[0]['icone'];
+$ativo_sistema = $res[0]['ativo'];
+
+if($ativo_sistema != 'Sim' and $ativo_sistema != ''){ ?>
+	<style type="text/css">
+		@media only screen and (max-width: 700px) {
+  .imgsistema_mobile{
+    width:300px;
+  }
+    
+}
+	</style>
+	<div style="text-align: center; margin-top: 100px">
+	<img src="img/bloqueio.png" class="imgsistema_mobile">	
+	</div>
+<?php 
+exit();
+} 
+
+}	
+ ?>
--- index.php
+++ index.php
@@ -1,11 +1,36 @@
+<?php 
+require_once("conexao.php");
+
+$query = $pdo->query("SELECT * from usuarios");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$linhas = @count($res);
+$senha = '123';
+$senha_crip = sha1($senha);
+if($linhas == 0){
+	$pdo->query("INSERT INTO usuarios SET nome = '$nome_sistema', email = '$email_sistema', senha = '', senha_crip = '$senha_crip', nivel = 'Administrador', ativo = 'Sim', foto = 'sem-foto.jpg', telefone = '$telefone_sistema', data = curDate() ");
+}
+
+ ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nome do sistema</title>
 </head>
 <body>
-    <h1>djkldjakjdk</h1>
+    <div class="login">		
+		<div class="form">
+			<img src="img/logo.png" class="imagem">
+			<form method="post" action="autenticar.php">
+				<input type="email" name="usuario" placeholder="Seu Email" required>
+				<input type="password" name="senha" placeholder="Senha" required>
+				<button>Login</button>
+			</form>	
+			<br>
+			<p class="recuperar"><a title="Clique para recupearar a senha" href="" data-bs-toggle="modal" data-bs-target="#exampleModal">Recuperar Senha</a></p>
+
+		</div>
+	</div>
 </body>
 </html>
